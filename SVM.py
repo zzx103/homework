e20d38c941fb87b3bc0f6303962f594f88c788b3
zzx103 <zx19951025@qq.com>
2020-05-25 11:13:51 +0800

    update SVM

--- SVM.py
+++ SVM.py
@@ -2,31 +2,29 @@ import numpy as np
 
 
 class SVM:
-    def __init__(self, x_s, y_s):
-        self.N, self.x_n = x_s.shape
-        self.X = x_s
-        self.Y = y_s
+    def __init__(self, t_data, labels):
+        self.d_num, self.f_num = t_data.shape
+        self.x = t_data
+        self.y = labels
         self.b = 0.0
-        self.alpha = np.ones(self.N)
-
-        self.E = [self._E(i) for i in range(self.N)]
-
+        self.alpha = np.ones(self.d_num)
+        self.E = [self._E(i) for i in range(self.d_num)]
         self.C = 1.0
 
     def _K(self, xi, xj):
         return np.dot(xi, xj)
 
     def _E(self, i):
-        return self._g(i) - self.Y[i]
+        return self._g(i) - self.y[i]
 
     def _g(self, i):
-        r = self.b
-        for j in range(self.N):
-            r += self.alpha[j] * self.Y[j] * self._K(self.X[i], self.X[j])
-        return r
+        res = self.b
+        for j in range(self.d_num):
+            res += self.alpha[j] * self.y[j] * self._K(self.x[j], self.x[i])
+        return res
 
     def _KKT(self, i):
-        y_g = self._g(i) * self.Y[i]
+        y_g = self._g(i) * self.y[i]
         if self.alpha[i] == 0:
             return y_g >= 1
         elif 0 < self.alpha[i] < self.C:
@@ -34,7 +32,96 @@ class SVM:
         else:
             return y_g <= 1
 
+    def _choose_alpha(self):
+        i_a1 = -1
+        i_a2 = -1
+        for i in range(self.d_num):
+            if 0 < self.alpha[i] < self.C:
+                if not self._KKT(i):
+                    i_a1 = i
+                    break
+        if i_a1 == -1:
+            for i in range(self.d_num):
+                if not self._KKT(i):
+                    i_a1 = i
+                    break
+        if i_a1 == -1:
+            return i_a1, i_a2
+        if self.E[i_a1] > 0:
+            i_a2 = min(range(self.d_num), key=lambda x: self.E[x])
+        else:
+            i_a2 = max(range(self.d_num), key=lambda x: self.E[x])
+        return i_a1, i_a2
 
+    def train(self, i_times):
+        for t in range(i_times):
+            i_a1, i_a2 = self._choose_alpha()
+            if i_a1 == -1:
+                break
+            a1 = self.alpha[i_a1]
+            a2 = self.alpha[i_a2]
+            x1 = self.x[i_a1]
+            x2 = self.x[i_a2]
+            y1 = self.y[i_a1]
+            y2 = self.y[i_a2]
+            E1 = self.E[i_a1]
+            E2 = self.E[i_a2]
+
+            if y1 != y2:
+                L = max(0, a2 - a1)
+                H = min(self.C, self.C + a2 - a1)
+            else:
+                L = max(0, a2 + a1 - self.C)
+                H = min(self.C, a2 + a1)
+
+            eta = self._K(x1, x1) + self._K(x2, x2) - 2 * self._K(x1, x2)
+            a2_new_unc = a2 + y2 * (E1 - E2) / eta
+            if a2_new_unc > H:
+                a2_new = H
+            elif a2_new_unc < L:
+                a2_new = L
+            else:
+                a2_new = a2_new_unc
+            a1_new = a1 + y1 * y2 * (a2 - a2_new)
+            b1_new = -E1 - y1 * self._K(x1, x1) * (a1_new - a1) - y2 * self._K(x2, x1) * (a2_new - a2) + self.b
+            b2_new = -E2 - y1 * self._K(x1, x2) * (a1_new - a1) - y2 * self._K(x2, x2) * (a2_new - a2) + self.b
+
+            if 0 < a1_new < self.C:
+                b_new = b1_new
+            elif 0 < a2_new < self.C:
+                b_new = b2_new
+            else:
+                b_new = (b1_new + b2_new) / 2
+
+            # 更新参数
+            self.alpha[i_a1] = a1_new
+            self.alpha[i_a2] = a2_new
+            self.b = b_new
+
+            self.E[i_a1] = self._E(i_a1)
+            self.E[i_a2] = self._E(i_a2)
+
+
+    def predict(self, data):
+        r = self.b
+        for i in range(self.d_num):
+            r += self.alpha[i] * self.y[i] * self._K(data, self.x[i])
+
+        return 1 if r > 0 else -1
+
+    def score(self, X_test, y_test):
+        right_count = 0
+        for i in range(len(X_test)):
+            result = self.predict(X_test[i])
+            if result == y_test[i]:
+                right_count += 1
+        return right_count / len(X_test)
+
+    def _weight(self):
+        # linear model
+        yx = self.y.reshape(-1, 1) * self.x
+        self.w = np.dot(yx.T, self.alpha)
+        return self.w
 
 
 
